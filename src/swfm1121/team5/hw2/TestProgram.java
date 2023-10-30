718c638f3e59407a4e881696a54a19d970cc99fc
Angel Shau <shauangel2000@gmail.com>
2023-10-30 16:30:56 +0800

    update

--- src/swfm1121/team5/hw2/TestProgram.java
+++ src/swfm1121/team5/hw2/TestProgram.java
@@ -1,17 +1,18 @@
 package swfm1121.team5.hw2;
 public class TestProgram {
 
+    // Check if the target is the "Positive Integer Exponent" of the divider
+    // If "yes", return the exponent
+    // If not, return -1
     public int countExponent(int target, int divider){
-        int n = 0;
-        if(target <= 0){ return -1; }
-        while(target > 1){
-            if(target%divider == 0){
+        int n = 0;  // nth-power
+        if(target <= 0){ return -1; }        // Check if target is positive and > 0
+        while(target > 1){                   // Calculate n
+            if(target%divider == 0){         // Check if target is the multiple of divider
                 target/=divider;
-                n+=1;
+                n++;
             }
-            else {
-                return -1;
-            }
+            else { return -1; }              // If not, the target is not the power of the divider
         }
         return n;
     }
--- src/swfm1121/team5/hw2/TestProgramTest.java
+++ src/swfm1121/team5/hw2/TestProgramTest.java
@@ -1,16 +1,35 @@
 package swfm1121.team5.hw2;
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestProgramTest {
     @Test
-    void testCountExponent(){
+    void edgeCoverage(){
         TestProgram test = new TestProgram();
         assertEquals(-1, test.countExponent(-1, -1));
         assertEquals(-1, test.countExponent(3, 2));
         assertEquals(3, test.countExponent(8, 2));
     }
 
+    @Test
+    void edgePairCoverage(){
+        TestProgram test = new TestProgram();
+        assertEquals(-1, test.countExponent(-1, -1));
+        assertEquals(0, test.countExponent(1, 2));
+        assertEquals(-1, test.countExponent(3, 2));
+        assertEquals(3, test.countExponent(8, 2));
+    }
+
+    @Test
+    void pathCoverage(){
+        TestProgram test = new TestProgram();
+        assertEquals(-1, test.countExponent(4, 5));
+        assertEquals(0, test.countExponent(1, 2));
+        assertEquals(-1, test.countExponent(-1, 2));
+        assertEquals(-1, test.countExponent(12, 3));
+        assertEquals(2, test.countExponent(16, 4));
+    }
+
 }
+
